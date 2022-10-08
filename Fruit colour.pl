928cd5766f56130b53659eb6400420f2bbd69aa6
kadapala kiran kumar Reddy <113354223+kadapalakirankumarreddy143@users.noreply.github.com>
2022-10-08 09:15:23 +0530

    Update Fruit colour.pl

--- Fruit colour.pl
+++ Fruit colour.pl
@@ -1,5 +1,18 @@
-colour(cherry, red).
-colour(banana, yellow).
-colour(apple, red).
-colour(apple, green).
-colour(orange, orange).
+colour(red).
+colour(yellow).
+colour(red).
+colour(green).
+colour(orange).
+
+fruit(cherry).
+fruit(banana).
+fruit(apple).
+fruit(apple).
+fruit(orange).
+
+item(cherry,red).
+item(banana,yellow).
+
+
+find(X,Y):-item(X,Y),colour(Y).
+kiran(X,Y):-item(Y,X),fruit(Y).
